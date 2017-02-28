6c824da34d0b4890b88f399e87c3aec87da34e6d
JoshWinebrener <joshua.winebrener@gmail.com>
2017-02-27 19:47:18 -0600

    Added basic cookie-cutter code for home menu

--- app/src/main/java/com/losjyders/joshw/space_invaders/Game.java
+++ app/src/main/java/com/losjyders/joshw/space_invaders/Game.java
@@ -2,12 +2,36 @@ package com.losjyders.joshw.space_invaders;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Game extends AppCompatActivity {
 
+    // Create an anonymous implementation of OnClickListener
+    OnClickListener playListener = new OnClickListener() {
+
+        public void onClick(View v) {
+            // do something when the button is clicked
+        }
+
+    };
+
+    //add other listeners HERE
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        // Capture our button from layout
+        Button button = (Button)findViewById(R.id.button);
+        // Register the onClick listener with the implementation above
+        button.setOnClickListener(playListener);
+
+        //implement other listeners HERE
+
     }
+
 }
